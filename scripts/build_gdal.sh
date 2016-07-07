754d74e6080c90b09d35fe7722da9e9f432861b2
david.w.bitner <david.w.bitner@noaa.gov>
2016-07-07 14:20:10 -0500

    add configure parameters for geopackage and wfs to gdal build

--- scripts/build_gdal.sh
+++ scripts/build_gdal.sh
@@ -2,7 +2,7 @@ sudo apt-get -qq -y update
 sudo apt-get -y install git build-essential python-dev libproj-dev
 cd /tmp
 wget http://download.osgeo.org/geos/geos-3.4.2.tar.bz2
-tar xjf geos-3.4.2.tar.bz2
+tar xjf geos-3.4.2.tar.bz2c
 cd geos-3.4.2
 ./configure
 sudo make -j 4
@@ -10,9 +10,9 @@ sudo make install
 
 git clone https://github.com/OSGeo/gdal.git /srv/gdal
 cd /srv/gdal/gdal
-./configure --with-python --with-sqlite3 --with-spatialite --with-geopackage
+./configure --with-python --with-sqlite3 --with-spatialite --with-geopackage --with-curl
 #./mkbindist.sh -dev $version linux
-sed -i 's/#!\/bin\/sh/#!\/bin\/bash/'  mkbindist.sh
+sed -i 's/#!\/bin\/sh/#!\/bin\/bash/'  mkbindist.shcd 
 version=$(cat VERSION)
 ./mkbindist.sh -dev $version linux
 cp gdal-${version}-linux-bin.tar.gz /vagrant
